96328559fde6143ce7f3bf342db2b96535e7c90b
taeil00 <taeil00@naver.com>
2020-01-01 22:44:14 +0900

    update 및 update test부분 추가

--- src/main/java/org/taeil/springboot/service/PostsService.java
+++ src/main/java/org/taeil/springboot/service/PostsService.java
@@ -2,8 +2,11 @@ package org.taeil.springboot.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.taeil.springboot.domain.posts.Posts;
 import org.taeil.springboot.domain.posts.PostsRepository;
+import org.taeil.springboot.web.dto.PostsResponseDto;
 import org.taeil.springboot.web.dto.PostsSaveRequestDto;
+import org.taeil.springboot.web.dto.PostsUpdateRequestDto;
 
 import javax.transaction.Transactional;
 
@@ -16,5 +19,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+            new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
