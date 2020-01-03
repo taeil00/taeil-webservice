ba5ba507b203bbfc101ed91d329e7289d9d3900d
taeil00 <taeil00@naver.com>
2020-01-04 01:32:25 +0900

    LoginUser 애노테이션 사용하도록 개선, LoginUser애노테이션 정의시 Target패키지 경로 수정

--- src/main/java/org/taeil/springboot/config/auth/LoginUser.java
+++ src/main/java/org/taeil/springboot/config/auth/LoginUser.java
@@ -1,10 +1,11 @@
 package org.taeil.springboot.config.auth;
 
-import org.hibernate.annotations.Target;
 
+import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+
 
 
 @Target(ElementType.PARAMETER)
--- src/main/java/org/taeil/springboot/web/IndexController.java
+++ src/main/java/org/taeil/springboot/web/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.taeil.springboot.config.auth.LoginUser;
 import org.taeil.springboot.config.auth.dto.SessionUser;
 import org.taeil.springboot.domain.user.User;
 import org.taeil.springboot.service.PostsService;
@@ -20,10 +21,10 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
